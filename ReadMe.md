9e677ccf86824b7239121bd926565c50aa771458
xc145214 <xc145214@gmail.com>
2016-12-19 10:24:26 +0800

    垃圾回收

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,11 @@
 # JVM 高级特性与最佳实践
 
++ 常用 JVM 参数及配置含义
+    + [x] [JVM 参数配置](DOC/00.jvm-args-config.md)
 + Java 内存区域和内存区域异常
     + [x] [运行时数据区域](DOC/01.runtime-data-region.md)
     + [x] [HotSpot 虚拟机对象探秘](DOC/01.hotspot-vm-object.md)
     + [x] [OutOfMemoryError 异常](DOC/01.hotspot-vm-object.md)
++ 垃圾收集器和内存分配策略
+    + [x] [对象已死？](DOC/02.object-dead.md)
+
--- jvm/src/main/java/com/xc/gc/ReferenceCountingGC.java
+++ jvm/src/main/java/com/xc/gc/ReferenceCountingGC.java
@@ -12,12 +12,45 @@
  *************************************************************************/
 package com.xc.gc;
 
+
 /**
- * XXXXXXXXXXXXXXXXXXXXX
+ *
  *
  * @author xiachuan at 2016/12/19 9:52。
  */
 
 public class ReferenceCountingGC {
+
+    public Object instance = null;
+
+    private static final  int _1MB = 1024 * 1024;
+
+    /**
+     * 此成员变量的意义在于占用内存，方便能在gc日志中查看是否被回收。
+     */
+    private byte[] bigSize = new byte[2* _1MB];
+
+    public static void testGC(){
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA = null;
+        objB = null;
+
+        //调用GC
+        System.gc();
+
+
+    }
+
+
+    public static void main(String[] args) {
+        testGC();
+    }
+
+
 }
 
